b546940604bce35c9c8a980a83f6388131fac843
rephaelaz <raphalaz@campus.technion.ac.il>
2018-04-13 14:33:20 +0300

    DB: Added faculty and study program models. Added basic models for faculties and study program, containing names and relation between them. #3

--- website/scheduler/models.py
+++ website/scheduler/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# General course database, common to all the faculties
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class StudyProgram(models.Model):
+    name = models.CharField(max_length=200)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+
+
