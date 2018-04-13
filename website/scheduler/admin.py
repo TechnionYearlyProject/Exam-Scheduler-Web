1ecddd7483507f630df4a0d2e234de76222b96dc
rephaelaz <raphalaz@campus.technion.ac.il>
2018-04-13 15:23:56 +0300

    DB: Added admin view + models update - Added admin view to manage database content. - Added ordering and string formatting to models (for the admin view) #3 #6

--- website/scheduler/admin.py
+++ website/scheduler/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Faculty)
+admin.site.register(StudyProgram)
+admin.site.register(Course)
+admin.site.register(Registration)
--- website/scheduler/models.py
+++ website/scheduler/models.py
@@ -1,21 +1,34 @@
 from django.db import models
 
+
 # General course database, common to all the faculties
 
 class Faculty(models.Model):
     name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
 
 
 class StudyProgram(models.Model):
     name = models.CharField(max_length=200)
     faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ['name', 'faculty']
+
+    def __str__(self):
+        return '{0} - {1}'.format(self.name, str(self.faculty))
+
 
 class Course(models.Model):
     ID = models.CharField(primary_key=True, max_length=6)
     name = models.CharField(max_length=200)
     faculty = models.ForeignKey(Faculty, on_delete=models.SET_NULL, null=True)
-    credit_points = models.FloatField()
+    credit_points = models.DecimalField(max_digits=2, decimal_places=1)
 
     # Algorithm flags
     days_before = models.IntegerField()
@@ -27,9 +40,17 @@ class Course(models.Model):
     # Internal flags
     is_taught = models.BooleanField(default=True)
 
+    class Meta:
+        ordering = ['ID']
+
+    def __str__(self):
+        return "{0} - {1}".format(self.name, self.ID)
+
 
 class Registration(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     study_program = models.ForeignKey(StudyProgram, on_delete=models.CASCADE)
     semester = models.IntegerField()
 
+    def __str__(self):
+        return "{0} - {1}".format(str(self.course), str(self.study_program))
