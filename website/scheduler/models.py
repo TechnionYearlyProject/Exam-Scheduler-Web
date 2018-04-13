8eeb73ab4c4aed4cacebb36f6e0903969da23a8a
rephaelaz <raphalaz@campus.technion.ac.il>
2018-04-13 14:36:43 +0300

    DB: Added course model. Added model for course. The model contains data available for the user as well as flags used to configure the scheduling algorithm. #3

--- website/scheduler/models.py
+++ website/scheduler/models.py
@@ -10,4 +10,18 @@ class StudyProgram(models.Model):
     name = models.CharField(max_length=200)
     faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
 
+class Course(models.Model):
+    ID = models.CharField(primary_key=True, max_length=6)
+    name = models.CharField(max_length=200)
+    faculty = models.ForeignKey(Faculty, on_delete=models.SET_NULL)
+    credit_points = models.FloatField()
 
+    # Algorithm flags
+    days_before = models.IntegerField()
+    is_required = models.BooleanField(default=True)
+    has_exam = models.BooleanField(default=True)
+    is_first = models.BooleanField(default=False)
+    is_last = models.BooleanField(default=False)
+
+    # Internal flags
+    is_taught = models.BooleanField(default=True)
