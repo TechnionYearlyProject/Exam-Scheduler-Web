0d9d5410366edf4a4f525082429e9ec02d5a32d8
rephaelaz <raphalaz@campus.technion.ac.il>
2018-04-15 18:32:40 +0300

    Removed django project.

--- website/scheduler/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-
-admin.site.register(Faculty)
-admin.site.register(StudyProgram)
-admin.site.register(Course)
-admin.site.register(Registration)
