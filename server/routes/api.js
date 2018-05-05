2da17dffdddbcf5fa261abca1e28be8df8b521a5
ucfBader <yose981@gmail.com>
2018-05-06 00:55:03 +0300

    DB: added course API. added: api/course/list:  shows all courses. api/faculty/:id/course/list: shows faculty courses. api/faculty/:id/course/create: creates new course. #34 #33

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,23 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const faculty_controller = require('../controllers/facultyController');
-const study_program_controler = require('../controllers/studyprogramController');
-// const schedule_controller = require('../controllers/scheduleController');
+const study_program_controller = require('../controllers/studyprogramController');
+const schedule_controller = require('../controllers/scheduleController');
+const course_controller = require('../controllers/courseController');
 
 router.get('/', (req, res) => {
   res.send('api works');
 });
 
+
 router.get('/faculty/list', faculty_controller.faculty_list);
 router.post('/faculty/create', faculty_controller.faculty_create);
 router.delete('/faculty/delete', faculty_controller.faculty_delete);
 
-router.get('/program/list', study_program_controler.study_program_list); // All faculties combined
-router.get('/faculty/:id/program/list', study_program_controler.study_program_list_by_faculty); // Specific fac
-router.post('/faculty/:id/program/create', study_program_controler.study_program_create);
-router.delete('/faculty/:id/program/delete', study_program_controler.study_program_delete);
+router.get('/program/list', study_program_controller.study_program_list); // All faculties combined
+router.get('/faculty/:id/program/list', study_program_controller.study_program_list_by_faculty); // Specific fac
+router.post('/faculty/:id/program/create', study_program_controller.study_program_create);
+router.delete('/faculty/:id/program/delete', study_program_controller.study_program_delete);
 
-// router.get('/schedule/list', schedule_controller.schedule_list);
+
+router.get('/course/list', course_controller.course_list);//all courses
+router.get('/faculty/:id/course/list',course_controller.faculty_course_list);//faculty courses
+
+//router.put('/faculty/:id/course/:courseID/update',course_controller.faculty_course_update);
+
+router.post('/faculty/:id/course/create',course_controller.course_create);
+
+
+
+//router.get('/schedule/list', schedule_controller.schedule_list);
 // router.post('/schedule/create', schedule_controller.schedule_create);
 
 module.exports = router;
