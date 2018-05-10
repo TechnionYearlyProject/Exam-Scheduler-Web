fc65546749110facd8149bd852dbc7bafc6128d4
rephaelaz <raphalaz@campus.technion.ac.il>
2018-05-10 10:44:42 +0300

    DB: Added User model. Added model to represent users, containing user, email and password. #5

---
+++ server/models/user.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    password: {
+      type: String,
+      unique: true,
+      required: true
+    }
+  }
+);
+
+module.exports = mongoose.model('User', UserSchema);
