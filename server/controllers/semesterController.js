cf91c5a5f96dca81525d1aa9f13e2d4a40e1ecc1
rephaelaz <raphalaz@campus.technion.ac.il>
2018-05-23 10:09:38 +0300

    DB: Added Semester controller. Added API method to add, update and get list of semesters. #50

---
+++ server/controllers/semesterController.js
@@ -0,0 +1,48 @@
+const Semester = require('../models/semester').model;
+
+exports.semester_list = function (req, res, next) {
+  Semester.find({}, 'year semester')
+  .then(data => {
+    return res.json(data)
+  })
+  .catch(err => {
+    next(err);
+  })
+};
+
+exports.semester_create = function (req, res, next) {
+  Semester.create({
+    year: req.body.year,
+    semester: req.body.semester,
+    start_a: req.body.start_a,
+    end_a: req.body.end_a,
+    start_b: req.body.start_b,
+    end_b: req.body.end_b
+  })
+  .then(() => {
+    return res.end();
+  })
+  .catch(err => {
+    next(err);
+  })
+};
+
+exports.semester_update_dates = function (req, res, next) {
+  const condition = {
+    year: req.body.year,
+    semester: req.body.semester
+  };
+  const update = {
+    start_a: req.body.start_a,
+    end_a: req.body.end_a,
+    start_b: req.body.start_b,
+    end_b: req.body.end_b
+  };
+  Semester.findOneAndUpdate(condition, update)
+  .then(() => {
+    return res.end();
+  })
+  .catch(err => {
+    next(err);
+  })
+};
