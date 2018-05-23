8743ffab365680e0d0eab686cc2fb347443cf458
rephaelaz <raphalaz@campus.technion.ac.il>
2018-05-24 00:52:53 +0300

    Server: Updated Schedule API. * Added request to create new Schedule. #51

--- server/controllers/scheduleController.js
+++ server/controllers/scheduleController.js
@@ -8,9 +8,9 @@ exports.schedule_list = function(req,res,next){
   })
   .then(semester => {
     if (!semester) {
-      return next(new Error('Semester not found'));
+      return res.status(404).send('Semester not found.');
     }
-    return Schedule.find ({semester: semester._id}, 'name faculty start end exams');
+    return Schedule.find({semester: semester._id}, 'name faculty start end exams');
   })
   .then(schedule => {
     return res.json(schedule);
@@ -20,23 +20,44 @@ exports.schedule_list = function(req,res,next){
   });
 };
 
-exports.faculty_schedule = function(req,res,next){
+exports.faculty_schedule = function(req,res,next) {
   Semester.findOne({
     year: req.params.year,
     semester: req.params.semester
   })
   .then(semester => {
     if (!semester) {
-      return next(new Error('Semester not found'));
+      return res.status(404).send('Semester not found.');
     }
-    return Schedule.find ({
+    return Schedule.find({
       semester: semester._id,
       faculty: req.faculty_id
-    },
-    'name faculty start end exams');
+    });
   })
   .then(schedule => {
     return res.json(schedule);
+  })
+  .catch(err => {
+    next(err);
+  });
+};
+
+exports.add_schedule = function (req, res, next) {
+  Semester.findOne({
+    year: req.params.year,
+    semester: req.params.semester
+  })
+  .then(semester => {
+    if (!semester) {
+      return res.status(404).send('Semester not found.');
+    }
+    return Schedule.create({
+      semester: semester._id,
+      faculty: req.faculty_id,
+    });
+  })
+  .then(() => {
+    return res.end();
   })
   .catch(err => {
     next(err);
