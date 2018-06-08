7aa0b822f30ea3b9ddc0451c3e8f4c461963ca1b
ucfBader <yose981@gmail.com>
2018-06-08 22:31:22 +0300

    DB: added functionality for assigning course to schedule, and clearing a schedule.

--- server/controllers/scheduleController.js
+++ server/controllers/scheduleController.js
@@ -43,3 +43,69 @@ exports.faculty_schedule = async function(req,res,next) {
   }
   return res.json(schedule);
 };
+
+exports.schedule_course = async function(req, res, next){
+    const semester = await Semester.findOne({
+        year: req.params.year,
+        semester: req.params.semester
+    }).catch(err => {next(err)});
+    if (!semester) {
+        return res.status(404).send('Semester not found.');
+    }
+    let schedule = await Schedule.findOne({
+        semester: semester._id,
+        //faculty: req.faculty_id
+    }).catch(err => {next(err)});
+    if (!schedule) {
+        return res.status(404).send('Schedule not found.');
+    }
+
+    let course = await Course.findOne({
+        id: req.params.courseID,
+        semester: semester._id
+    }).catch(err => {next(err)});
+    if (!course) {
+        return res.status(404).send('Course not found.');
+    }
+    var dates = {course:course._id, date_a:req.body.date_a,date_b:req.body.date_b};
+    await Schedule.update({ semester: semester._id },
+        { $push: { exams: dates } },function (error, success) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(success);
+            }
+        }
+    );
+    await schedule.exams.push({course:req.params.id, date_a: req.body.date_a,date_b:req.body.date_b});
+    //return res.end();
+};
+
+exports.clearExams = async function(req,res,next){
+
+    const semester = await Semester.findOne({
+        year: req.params.year,
+        semester: req.params.semester
+    }).catch(err => {next(err)});
+    if (!semester) {
+        return res.status(404).send('Semester not found.');
+    }
+    let schedule = await Schedule.findOne({
+        semester: semester._id,
+        //faculty: req.faculty_id
+    }).catch(err => {next(err)});
+    if (!schedule) {
+        return res.status(404).send('Schedule not found.');
+    }
+
+    await Schedule.update({ semester: semester._id },
+        { $set: { exams: [] } },function (error, success) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(success);
+            }
+        }
+    );
+    await schedule.exams.push({course:req.params.id, date_a: req.body.date_a,date_b:req.body.date_b});
+};
