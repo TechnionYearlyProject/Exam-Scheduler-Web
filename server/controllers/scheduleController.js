7ace349d8e93d3849bb00c42008798a586ab679d
ucfBader <yose981@gmail.com>
2018-06-12 02:53:08 +0300

    DB: Schedule contains two arrays one for moed a and the other for moed b, you can assign courses separately (see schedule_moed_a/b routes) . #63

--- server/controllers/scheduleController.js
+++ server/controllers/scheduleController.js
@@ -1,6 +1,6 @@
 const Schedule = require('../models/schedule').model;
 const Semester = require('../models/semester').model;
-
+const Course = require('../models/course').model;
 exports.schedule_list = function(req,res,next){
   Semester.findOne({
     year: req.params.year,
@@ -44,7 +44,7 @@ exports.faculty_schedule = async function(req,res,next) {
   return res.json(schedule);
 };
 
-exports.schedule_course = async function(req, res, next){
+exports.schedule_moed_a = async function(req, res, next){
     const semester = await Semester.findOne({
         year: req.params.year,
         semester: req.params.semester
@@ -53,8 +53,7 @@ exports.schedule_course = async function(req, res, next){
         return res.status(404).send('Semester not found.');
     }
     let schedule = await Schedule.findOne({
-        semester: semester._id,
-        //faculty: req.faculty_id
+        semester: semester._id, faculty:req.faculty_id
     }).catch(err => {next(err)});
     if (!schedule) {
         return res.status(404).send('Schedule not found.');
@@ -67,22 +66,26 @@ exports.schedule_course = async function(req, res, next){
     if (!course) {
         return res.status(404).send('Course not found.');
     }
-    var dates = {course:course._id, date_a:req.body.date_a,date_b:req.body.date_b};
-    await Schedule.update({ semester: semester._id },
-        { $push: { exams: dates } },function (error, success) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log(success);
-            }
-        }
-    );
-    await schedule.exams.push({course:req.params.id, date_a: req.body.date_a,date_b:req.body.date_b});
-    //return res.end();
+    var dates = {course:course._id, date:req.body.date};
+    // schedule.exams_a.push(dates);
+    await Schedule.update(
+        { semester: semester._id, faculty:req.faculty_id},
+        { $push: { exams_a: dates } },
+        function (error, success) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log(success);
+                    }
+                }
+            ).then(() => {
+                return res.end();
+            }).catch(err => {
+                    next(err);
+                });
 };
 
-exports.clearExams = async function(req,res,next){
-
+exports.schedule_moed_b = async function(req, res, next){
     const semester = await Semester.findOne({
         year: req.params.year,
         semester: req.params.semester
@@ -91,21 +94,61 @@ exports.clearExams = async function(req,res,next){
         return res.status(404).send('Semester not found.');
     }
     let schedule = await Schedule.findOne({
-        semester: semester._id,
-        //faculty: req.faculty_id
+        semester: semester._id, faculty:req.faculty_id
     }).catch(err => {next(err)});
     if (!schedule) {
         return res.status(404).send('Schedule not found.');
     }
 
+    let course = await Course.findOne({
+        id: req.params.courseID,
+        semester: semester._id
+    }).catch(err => {next(err)});
+    if (!course) {
+        return res.status(404).send('Course not found.');
+    }
+    var dates = {course:course._id, date:req.body.date};
     await Schedule.update({ semester: semester._id },
-        { $set: { exams: [] } },function (error, success) {
+        { $push: { exams_b: dates } },function (error, success) {
             if (error) {
                 console.log(error);
             } else {
                 console.log(success);
             }
         }
-    );
-    await schedule.exams.push({course:req.params.id, date_a: req.body.date_a,date_b:req.body.date_b});
+    ).then(() => {
+        return res.end();
+    }).catch(err => {
+        next(err);
+    });
+};
+
+exports.clear_exams = async function(req, res, next){
+    const semester = await Semester.findOne({
+        year: req.params.year,
+        semester: req.params.semester
+    }).catch(err => {next(err)});
+    if (!semester) {
+        return res.status(404).send('Semester not found.');
+    }
+    let schedule = await Schedule.findOne({
+        semester: semester._id, faculty:req.faculty_id
+    }).catch(err => {next(err)});
+    if (!schedule) {
+        return res.status(404).send('Schedule not found.');
+    }
+
+    await Schedule.update({ semester: semester._id },
+        { $set: { exams_a: [],exams_b:[] } },function (error, success) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(success);
+            }
+        }
+    ).then(() => {
+        return res.end();
+    }).catch(err => {
+        next(err);
+    });
 };
