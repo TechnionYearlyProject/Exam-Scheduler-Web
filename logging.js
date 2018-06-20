9776f3db825114e4793f455800c0bff61423e15a
rephaelaz <raphalaz@campus.technion.ac.il>
2018-06-20 16:06:17 +0300

    Server: Added logging messages calls. * Added logging to login controller. * Added logging to semester controller. * Added logging to faculty controller. #1

--- logging.js
+++ logging.js
@@ -1,3 +1,4 @@
 exports.info = require('debug')('server:info');
+exports.db = require('debug')('server:database');
 exports.warning = require('debug')('server:warning');
 exports.error = require('debug')('server:error');
