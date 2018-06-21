a77524962c0f0bd1191af618b47a718b38a581a4
ucfBader <yose981@gmail.com>
2018-06-21 23:14:01 +0300

    Tests: tests for Schedule module. #57

--- server/controllers/scheduleController.js
+++ server/controllers/scheduleController.js
@@ -1,24 +1,24 @@
 const Schedule = require('../models/schedule').model;
 const Semester = require('../models/semester').model;
 const Course = require('../models/course').model;
-exports.schedule_list = function (req, res, next) {
-  Semester.findOne({
-    year: req.params.year,
-    semester: req.params.semester
-  })
-  .then(semester => {
-    if (!semester) {
-      return res.status(404).send('Semester not found.');
-    }
-    return Schedule.find({semester: semester._id}, 'name faculty start end exams');
-  })
-  .then(schedule => {
-    return res.json(schedule);
-  })
-  .catch(err => {
-    next(err);
-  });
-};
+// exports.schedule_list = function (req, res, next) {
+//   Semester.findOne({
+//     year: req.params.year,
+//     semester: req.params.semester
+//   })
+//   .then(semester => {
+//     if (!semester) {
+//       return res.status(404).send('Semester not found.');
+//     }
+//     return Schedule.find({semester: semester._id}, 'name faculty start end exams');
+//   })
+//   .then(schedule => {
+//     return res.json(schedule);
+//   })
+//   .catch(err => {
+//     next(err);
+//   });
+// };
 
 exports.faculty_schedule = async function (req, res, next) {
   const semester = await Semester.findOne({
@@ -57,9 +57,10 @@ exports.schedule_moed_a = async function (req, res, next) {
   }).catch(err => {
     next(err)
   });
-  if (!semester) {
-    return res.status(404).send('Semester not found.');
-  }
+    if(!semester){
+        return res.status(404).send('Semester not found.');
+    }
+
   let schedule = await Schedule.findOne({
     semester: semester._id, faculty: req.faculty_id
   }).catch(err => {
@@ -82,7 +83,6 @@ exports.schedule_moed_a = async function (req, res, next) {
     return res.status(401).send('Date missing.');
   }
   var dates = {course: course._id, date: req.body.date};
-  // schedule.exams_a.push(dates);
   await Schedule.update(
   {semester: semester._id, faculty: req.faculty_id},
   {$push: {exams_a: dates}},
