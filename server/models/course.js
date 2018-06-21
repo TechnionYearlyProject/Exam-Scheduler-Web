085af8bc2b3db39632c3b303dedbbca18a01110d
rephaelaz <raphalaz@campus.technion.ac.il>
2018-06-21 18:21:30 +0300

    DB: Auto-update database when removing course. * Remove conflict from other courses of the same semester. * Remove schedule of the same faculty and same semester. * Remove message list. #84

--- server/models/course.js
+++ server/models/course.js
@@ -1,79 +1,115 @@
 const mongoose = require('mongoose');
+const Schedule = require('./schedule').model;
+const MessageList = require('./messagelist').model;
 
 const CourseSchema = new mongoose.Schema({
-    id: {
-        type: Number,
-        required: true,
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    faculty: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'Faculty',
-        required: true
-    },
-    credit_point: {
-        type: mongoose.Schema.Types.Decimal128,
-        required: true,
-        default: 3,
-        min: 0
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  faculty: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Faculty',
+    required: true
+  },
+  credit_point: {
+    type: mongoose.Schema.Types.Decimal128,
+    required: true,
+    default: 3,
+    min: 0
+  },
+  semester: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Semester',
+    required: true
+  },
+
+  registrations: [{
+    study_program: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'StudyProgram'
     },
     semester: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'Semester',
-        required: true
-    },
-
-    registrations: [{
-        study_program: {
-            type: mongoose.Schema.ObjectId,
-            ref: 'StudyProgram'
-        },
-        semester: {
-            type: Number,
-            min: 1
-        }
-    }],
-
-    conflicts: [{
-        course: {
-            type: mongoose.Schema.ObjectId,
-            ref: 'Course'
-        },
-    }],
-    is_required: {
-        type: Boolean,
-        default: true
-    },
-    has_exam: {
-        type: Boolean,
-        default: true
-    },
-    constraint_a: {
-        type: Date,
-        default: null
-    },
-
-    constraint_b: {
-        type: Date,
-        default: null
-    },
-    // Algorithm flags
-    days_before: {
-        type: Number,
-        default: 3,
-        min: 1
-    },
-    is_first: {
-        type: Boolean,
-        default: false
-    },
-    is_last: {
-        type: Boolean,
-        default: false
+      type: Number,
+      min: 1
     }
+  }],
+
+  conflicts: [{
+    course: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Course'
+    },
+  }],
+  is_required: {
+    type: Boolean,
+    default: true
+  },
+  has_exam: {
+    type: Boolean,
+    default: true
+  },
+  constraint_a: {
+    type: Date,
+    default: null
+  },
+
+  constraint_b: {
+    type: Date,
+    default: null
+  },
+  // Algorithm flags
+  days_before: {
+    type: Number,
+    default: 3,
+    min: 1
+  },
+  is_first: {
+    type: Boolean,
+    default: false
+  },
+  is_last: {
+    type: Boolean,
+    default: false
+  }
+});
+
+CourseSchema.pre('remove', function (next) {
+  this.model('Course').update({
+    semester: this.semester
+  }, {
+    $pull: {
+      conflicts: this._id
+    }
+  }).exec();
+  Schedule.update({
+    faculty: this.faculty,
+    semester: this.semester
+  }, {
+    $pull: {
+      exams_a: {
+        course: this._id
+      }
+    }
+  }).exec();
+  Schedule.update({
+    faculty: this.faculty,
+    semester: this.semester
+  }, {
+    $pull: {
+      exams_b: {
+        course: this._id
+      }
+    }
+  }).exec();
+  MessageList.remove({
+    course: this._id
+  }).exec();
+  next();
 });
 
 exports.model = mongoose.model('Course', CourseSchema);
