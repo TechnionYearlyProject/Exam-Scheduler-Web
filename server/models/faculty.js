e76b92f6660db53c192b18128a38b29cd428efee
rephaelaz <raphalaz@campus.technion.ac.il>
2018-06-21 18:59:10 +0300

    DB: Auto-update database when removing faculty. * Remove all courses of the faculty. * Remove all schedules of the faculty. * Remove all messages associated with this course. * Update list of faculties of study programs. #84

--- server/models/faculty.js
+++ server/models/faculty.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const Course = require('./course').model;
+const Schedule = require('./schedule').model;
+const StudyProgram = require('./studyprogram').model;
+const MessageList = require('./messagelist').model;
 
 const FacultySchema = new mongoose.Schema({
   name: {
@@ -20,6 +24,31 @@ const FacultySchema = new mongoose.Schema({
     type: String,
     required: true
   }
+});
+
+FacultySchema.pre('remove', function (next) {
+  Course.remove({
+    faculty: this._id
+  }).exec();
+  Schedule.remove({
+    faculty: this._id
+  }).exec();
+  MessageList.remove({
+    faculty: this._id
+  }).exec();
+  MessageList.update({}, {
+    $pull: {
+      messages: {
+        sender: this._id
+      }
+    }
+  }).exec();
+  StudyProgram.update({}, {
+    $pull: {
+      faculty: this._id
+    }
+  }).exec();
+  next();
 });
 
 exports.model = mongoose.model('Faculty', FacultySchema);
